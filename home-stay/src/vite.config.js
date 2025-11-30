578af479fde1de30c7c57127ca130a7a46e3ed9b
Kaviya Sree <kaviya2506@gmail.com>
2025-11-30 16:09:09 +0530

    Update latest code for deployment

--- home-stay/src/vite.config.js
+++ home-stay/src/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base:process.env.VITE_BASE_PATH||"home-stay " ,
+  "scripts": {
+  "dev": "vite",
+  "build": "vite build",
+  "preview": "vite preview"
+}
+
   
 });
